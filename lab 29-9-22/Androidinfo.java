40c47b242041a2da43d8113c89bd97bfee3f3035
Gitanjali552 <113021724+Gitanjali552@users.noreply.github.com>
2022-09-29 18:22:30 +0530

    Add files via upload

---
+++ lab 29-9-22/Androidinfo.java
@@ -0,0 +1,16 @@
+package javacode;
+
+public class Androidinfo {
+
+	public static void main(String[] args) {
+		public static void main(String args[]){
+		    Android[] A =  new Android[3];
+		    A[0] = new Android(7 ,5, "volite");
+		    A[1] = new Android(8 ,12, "white");
+		    A[2] = new Android(9 ,6, "green");
+		    A[0].info();A[1].info();A[2].info();
+		    }
+
+	}
+
+}
---
+++ lab 29-9-22/android.java
@@ -0,0 +1,24 @@
+package javacode;
+
+public class android {
+
+	public static void main(String[] args) {
+		int modelno;
+	    int ram;
+		String colour;
+
+
+		public Android(int storage,int ram, String colour) {
+	        this.modelno = storage;
+	        this.ram = ram;
+	        this.colour = colour;
+	    }
+	    public void info(){
+	    	System.out.println(modelno +"'s colour is " + colour + " and Ram is "+ ram);
+	    }
+
+	}
+
+	}
+
+}
